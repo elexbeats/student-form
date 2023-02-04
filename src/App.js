f131b432925122ec3e08f81d3f70bec49f2fdc2c
elexbeats <118662929+elexbeats@users.noreply.github.com>
2023-02-04 09:48:12 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import MainContainer from "./components/MainContainer";
+import "./App.css";
+// import ShowStudent from "./components/ShowStudent";
+import UserForm from "./components/UserForm";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <UserForm />
+    </div>
+  );
+};
+
+export default App;
